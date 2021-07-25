8a6ebddf6ff5af2a7d3fd525f394c67ce8e8e6cf
Craig Miller <craigmiller160@gmail.com>
2021-07-25 14:27:29 -0400

    Renamed config class

--- src/main/java/io/craigmiller160/csrf/spring/SpringTomcatCsrfFilterConfig.java
+++ src/main/java/io/craigmiller160/csrf/spring/SpringTomcatCsrfFilterConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class CsrfConfig {
+public class SpringTomcatCsrfFilterConfig {
 
     @Bean
     // TODO add docs for this property
