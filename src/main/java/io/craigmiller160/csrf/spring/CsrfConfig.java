af4c6124e03fc991d2fca925d19f311e7ddb74c7
Craig Miller <craigmiller160@gmail.com>
2021-07-25 11:00:46 -0400

    Finished the last of the filter configuration

--- src/main/java/io/craigmiller160/csrf/spring/CsrfConfig.java
+++ src/main/java/io/craigmiller160/csrf/spring/CsrfConfig.java
@@ -1,6 +1,7 @@
 package io.craigmiller160.csrf.spring;
 
 import org.apache.catalina.filters.RestCsrfPreventionFilter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +10,8 @@ import org.springframework.context.annotation.Configuration;
 public class CsrfConfig {
 
     @Bean
+    // TODO add docs for this property
+    @ConditionalOnProperty(value = "csrf.disable-csrf", havingValue = "false", matchIfMissing = true)
     public FilterRegistrationBean<RestCsrfPreventionFilter> restCsrfPreventionFilter() {
         final var filter = new RestCsrfPreventionFilter();
         filter.setDenyStatus(403);
